5e191d02bb6a7199e38ee045cd9ac5ed6b8c1b20
itbaby <jquery@hotmail.com>
2024-12-08 21:49:17 +0800

    add draft version

--- watcher.js
+++ watcher.js
@@ -1,18 +1,16 @@
 // External dependencies
 import { createReadStream, readFileSync } from "node:fs";
 import pino from "pino";
-import mitt from 'mitt';
 import walk from "walk";
 import knex from "knex";
+import { everySeries } from "async";
 import { parse } from "ini";
 import * as csv from 'fast-csv';
-import retry from "async-retry";
-import { sortBy, remove } from "lodash-es";
+import { sortBy } from "lodash-es";
 import { DownloaderHelper } from "node-downloader-helper";
 //config and init logger 
 const config = parse(readFileSync("./config.ini", "utf-8"));
 const logger = pino(pino.destination({ dest: './logs', sync: true }));
-const emitter = mitt();
 const { host, port, user, password, database } = config.database;
 let client = knex({
   client: 'pg',
@@ -20,11 +18,10 @@ let client = knex({
   pool: { min: 2, max: 10 }
 });
 
-
-let parseCsv2PG = (filePath, client) => {
+let parseCsv2PG = (filePath, callback) => {
   let chunkRecs = [];
-  console.log('----------------', filePath);
-  createReadStream(filePath)
+  const { name, mtime } = filePath;
+  createReadStream(name)
     .pipe(csv.parse({ headers: true }))
     .on('error', error => console.error(error))
     .on('data', row => {
@@ -36,23 +33,18 @@ let parseCsv2PG = (filePath, client) => {
     })
     .on('end', rows => {
       client.batchInsert('public.spams', chunkRecs, 1000).returning('number').then((numbers) => {
-        console.log('-------------');
         console.log(`Parsed ${numbers.length} rows, total ${rows} readed`);
+        callback(null, { fn: filePath });
       }).catch((e) => {
-        console.log(e);
-      }).finally(() => {
-        emitter.emit('end', { fn: filePath });
+        callback(e, null);
       });
-
     });
 
 }
 
 
 let files = []
-let counter = 0;
 const walker = walk.walk(config.path.watchdir);
-
 let handler = async (root, stats, next) => {
   files.push({
     name: `${root}\\${stats.name}`,
@@ -63,16 +55,9 @@ let handler = async (root, stats, next) => {
 
 walker.on("file", handler);
 walker.on("end", () => {
-  files = sortBy(files, "mtime");
-  files.map(({ name, mtime }) => {
-    parseCsv2PG(name, client);
-  })
-})
-
-emitter.on('end', (data) => {
-  if (++counter == files.length) {
+  everySeries(sortBy(files, "mtime"), parseCsv2PG, (err, result) => {
     process.exit(0);
-  }
+  });
 })
 
 /*
