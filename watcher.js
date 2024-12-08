05d873b3e82f0114b569bf035e1ad32a9397a8b1
itbaby <jquery@hotmail.com>
2024-12-08 21:18:20 +0800

    add more example

--- watcher.js
+++ watcher.js
@@ -1,42 +1,91 @@
 // External dependencies
-import fs from "fs";
-
-// Third-party packages
-import { client } from 'pg';
-import chokidar from "chokidar";
-import { format } from "date-fns";
-import ini from "ini";
+import { createReadStream, readFileSync } from "node:fs";
 import pino from "pino";
+import mitt from 'mitt';
+import walk from "walk";
+import knex from "knex";
+import { parse } from "ini";
+import * as csv from 'fast-csv';
 import retry from "async-retry";
-import { createStream } from "rotating-file-stream";
-
+import { sortBy, remove } from "lodash-es";
+import { DownloaderHelper } from "node-downloader-helper";
 //config and init logger 
-//
-const config = ini.parse(fs.readFileSync("./config.ini", "utf-8"));
-
-
-const stream = createStream("logs.txt", { size: "1K", interval: "1d", path: "./logs" });
-const logger = pino({ stream: stream });
-
-const pgClient = new client({
-  user: config.database.user,
-  host: config.database.host,
-  database: config.database.database,
-  password: config.database.password,
-  port: config.database.port,
+const config = parse(readFileSync("./config.ini", "utf-8"));
+const logger = pino(pino.destination({ dest: './logs', sync: true }));
+const emitter = mitt();
+const { host, port, user, password, database } = config.database;
+let client = knex({
+  client: 'pg',
+  connection: { host, port, user, password, database },
+  pool: { min: 2, max: 10 }
 });
 
-let parseRec2DB = async (path) => {
-  //parse record and insert to DB
-  //do logger for success and error
+
+let parseCsv2PG = (filePath, client) => {
+  let chunkRecs = [];
+  console.log('----------------', filePath);
+  createReadStream(filePath)
+    .pipe(csv.parse({ headers: true }))
+    .on('error', error => console.error(error))
+    .on('data', row => {
+      if (['MAYBE', 'PROBABLY', 'ALMOST_CERTAINLY', 'DEFINITELY'].includes(row['SpamScore'])) {
+        chunkRecs.push({
+          number: row['Number']
+        });
+      }
+    })
+    .on('end', rows => {
+      client.batchInsert('public.spams', chunkRecs, 1000).returning('number').then((numbers) => {
+        console.log('-------------');
+        console.log(`Parsed ${numbers.length} rows, total ${rows} readed`);
+      }).catch((e) => {
+        console.log(e);
+      }).finally(() => {
+        emitter.emit('end', { fn: filePath });
+      });
+
+    });
+
 }
 
-let process = async (path) => {
-  logger.error(`File ${path} was modified at ${format(Date.now(), "yyyy-MM-dd HH:mm:ss")}`);
-  logger.flush();
-  await retry(async (bail) => {
-    parseRec2DB(path);
-  }, { retries: 3 });
+
+let files = []
+let counter = 0;
+const walker = walk.walk(config.path.watchdir);
+
+let handler = async (root, stats, next) => {
+  files.push({
+    name: `${root}\\${stats.name}`,
+    mtime: stats.mtime
+  })
+  next();
 }
 
-chokidar.watch(config.path.watchdir).on('add', process).on('change', process);
+walker.on("file", handler);
+walker.on("end", () => {
+  files = sortBy(files, "mtime");
+  files.map(({ name, mtime }) => {
+    parseCsv2PG(name, client);
+  })
+})
+
+emitter.on('end', (data) => {
+  if (++counter == files.length) {
+    process.exit(0);
+  }
+})
+
+/*
+console.log(config.path.downloadURL);
+
+if (config.path.downloadURL) {
+  const downloader = new DownloaderHelper(config.path.downloadURL, config.path.downloadDir);
+  downloader.on('end', () => {
+    logger.info('Download completed');
+  });
+  downloader.on('error', (err) => {
+
+  })
+  downloader.start();
+}
+*/
